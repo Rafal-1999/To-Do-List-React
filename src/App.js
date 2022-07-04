eb417d331a50ac65bbb07c2b06147444f25e0ff1
Rafał <skapinka1999@wp.pl>
2022-07-04 22:06:20 +0200

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import Form from "./Form";
 
 function App() {
-	return (
-		// Homework from the module 6 and 7 Frontend Developer
-		<main>
-			<section className="list">
-				<h1 className="list__title">
-					Lista zadań
-				</h1>
-				<div className="list__block">
-					<div className="list__box">
-						<h2 className="list__header">
-							Dodaj nowe zadanie
-						</h2>
-					</div>
-					<div className="list__box list__box--no-line">
-						<Form />
-					</div>
-				</div>
-				<div className="list__block">
-					<div className="list__box list__box--flex">
-						<h2 className="list__header">
-							Lista zadań
-						</h2>
-						<div className="list__options"></div>
-					</div>
-					<div className="list__tasks-block">
-						<ul className="list__tasks"></ul>
-					</div>
-				</div>
-			</section>
-		</main>
-	);
+    return (
+        // Homework from the module 6 and 7 Frontend Developer
+        <main>
+            <section className="list">
+                <h1 className="list__title">
+                    Lista zadań
+                </h1>
+                <div className="list__block">
+                    <div className="list__box">
+                        <h2 className="list__header">
+                            Dodaj nowe zadanie
+                        </h2>
+                    </div>
+                    <div className="list__box list__box--no-line">
+                        <Form />
+                    </div>
+                </div>
+                <div className="list__block">
+                    <div className="list__box list__box--flex">
+                        <h2 className="list__header">
+                            Lista zadań
+                        </h2>
+                        <div className="list__options"></div>
+                    </div>
+                    <div className="list__tasks-block">
+                        <ul className="list__tasks"></ul>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default App;
