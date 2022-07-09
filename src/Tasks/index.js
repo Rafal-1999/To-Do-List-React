ae12010aaab76f07eba8e679506a71e5aa7d085c
Rafał <skapinka1999@wp.pl>
2022-07-09 21:03:12 +0200

    Created a tasks list view in Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,8 +1,19 @@
 const Tasks = (props) => (
     <ul className="list__tasks">
         {props.tasks.map(task => (
-            <li key={task.id}>
-                {task.content}
+            <li className={`list__tasks-item${task.done && props.hideDoneTasks ? " list__tasks-item--done" : ""}`}
+                key={task.id}>
+                <button className="list__action-button list__action-button--done">
+                    {`${task.done ? <i className="icon-check list__icon"></i> : ""}`}
+                </button>
+                <p className="list__paragraph">
+                    <span className={`list__tasks-content${task.done ? " list__tasks-content--done" : ""}`}>
+                        {task.content}
+                    </span>
+                </p>
+                <button className="list__action-button list__action-button--remove">
+                    <i className="icon-trash-empty list__icon"></i>
+                </button>
             </li>
         ))}
     </ul>
