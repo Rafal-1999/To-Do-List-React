142937fcfc2a1a180d7ea7d0f0ddfb56cc52244f
Rafał <skapinka1999@wp.pl>
2022-07-10 21:25:59 +0200

    Added icons

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,13 @@
+import "./fontello/css/fontello.css";
+import "./style.css";
+
 const Tasks = (props) => (
     <ul className="list__tasks">
         {props.tasks.map(task => (
             <li className={`list__tasks-item${task.done && props.hideDoneTasks ? " list__tasks-item--done" : ""}`}
                 key={task.id}>
                 <button className="list__action-button list__action-button--done">
-                    {`${task.done ? <i className="icon-check list__icon"></i> : ""}`}
+                    {task.done ? <i className="icon-check list__icon"></i> : ""}
                 </button>
                 <p className="list__paragraph">
                     <span className={`list__tasks-content${task.done ? " list__tasks-content--done" : ""}`}>
