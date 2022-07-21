5ef882520fb642acc8e53e3ef0521784e1c3ef4d
Rafał <skapinka1999@wp.pl>
2022-07-21 19:02:21 +0200

    Minimized condition in Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,10 +4,12 @@ import "./style.css";
 const Tasks = (props) => (
     <ul className="list__tasks">
         {props.tasks.map(task => (
-            <li className={`list__tasks-item${task.done && props.hideDoneTasks ? " list__tasks-item--done" : ""}`}
-                key={task.id}>
+            <li
+                className={`list__tasks-item${task.done && props.hideDoneTasks ? " list__tasks-item--done" : ""}`}
+                key={task.id}
+            >
                 <button className="list__action-button list__action-button--done">
-                    {task.done ? <i className="icon-check list__icon"></i> : ""}
+                    {task.done && <i className="icon-check list__icon" />}
                 </button>
                 <p className="list__paragraph">
                     <span className={`list__tasks-content${task.done ? " list__tasks-content--done" : ""}`}>
