47c8d6beec175a7b2ba3e77239fbe7b40f4411d0
Rafał <skapinka1999@wp.pl>
2022-11-14 22:18:33 +0100

    Created a tooltip for buttons in the task list

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,3 +1,4 @@
+import ReactTooltip from "react-tooltip";
 import "./fontello/css/fontello.css";
 import "./style.css";
 
@@ -17,10 +18,23 @@ const Tasks = ({
                 <button
                     type="button"
                     className="list__action-button list__action-button--done"
+                    data-for="action-button1"
+                    data-tip={`${task.done ? "Wznów" : "Ukończ"} zadanie`}
                     onClick={() => toggleDoneTask(task.id)}
                 >
                     {task.done && <i className="icon-check list__icon" />}
                 </button>
+                <ReactTooltip
+                    id="action-button1"
+                    className="list__tooltip"
+                    textColor="rgb(255, 255, 255)"
+                    backgroundColor="rgb(28, 28, 28)"
+                    arrowColor="rgb(28, 28, 28)"
+                    place="top"
+                    effect="solid"
+                    delayShow={200}
+                    delayHide={200}
+                />
                 <p className="list__paragraph">
                     <span
                         className={`list__tasks-content${task.done ? " list__tasks-content--done" : ""}`}
@@ -32,10 +46,23 @@ const Tasks = ({
                 <button
                     type="button"
                     className="list__action-button list__action-button--remove"
+                    data-for="action-button2"
+                    data-tip="Usuń zadanie"
                     onClick={() => removeTask(task.id)}
                 >
                     <i className="icon-trash-empty list__icon" />
                 </button>
+                <ReactTooltip
+                    id="action-button2"
+                    className="list__tooltip"
+                    textColor="rgb(255, 255, 255)"
+                    backgroundColor="rgb(28, 28, 28)"
+                    arrowColor="rgb(28, 28, 28)"
+                    place="top"
+                    effect="solid"
+                    delayShow={200}
+                    delayHide={200}
+                />
             </li>
         ))}
     </ul>
