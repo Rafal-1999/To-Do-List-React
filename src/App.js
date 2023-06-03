cd749489723480ec56b8566a19aef7a4a5a7e531
Rafał <skapinka1999@wp.pl>
2023-06-03 20:50:42 +0200

    Created hook useTasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
-import "./fontello/css/fontello.css";
+import { useTasks } from "./hooks/useTasks";
 import Section from "./components/Section";
 import Header from "./components/Header";
 import Content from "./components/Content";
@@ -9,6 +9,7 @@ import Form from "./components/Form";
 import Buttons from "./components/Buttons";
 import Tasks from "./components/Tasks";
 import Footer from "./components/Footer";
+import "./fontello/css/fontello.css";
 import { GlobalStyles } from "./globalStyles";
 import { lightTheme, darkTheme } from "./themes";
 
@@ -18,16 +19,6 @@ function App() {
         // JSON.parse(localStorage.getItem("darkMode") || false)
         JSON.parse(localStorage.getItem("theme") || false)
     );
-
-    const [tasks, setTasks] = useState(
-        JSON.parse(localStorage.getItem("tasks") || [])
-    );
-
-    const [hideDoneTasks, setHideDoneTasks] = useState(false);
-
-    useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }, [tasks]);
 
     useEffect(() => {
         // localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -51,57 +42,17 @@ function App() {
     //     }
     // };
 
-    const addNewTask = (taskContent) => {
-        setTasks(tasks => [
-            ...tasks, {
-                id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-                content: taskContent,
-                done: false
-            }
-        ])
-    };
-
-    const existTask = (taskContent) => {
-        if (tasks.some(({ content }) => content === taskContent)) {
-            alert("Podane zadanie jest już na liście.");
-            return true;
-        }
-    };
-
-    const hideAllDoneTasks = () => {
-        setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
-    };
-
-    const completeAllTasks = () => {
-        setTasks(tasks => tasks.map(task => ({
-            ...task,
-            done: true
-        })))
-    };
-
-    const toggleDoneTask = (id) => {
-        setTasks(tasks => tasks.map(task => {
-            if (task.id === id) {
-                return {
-                    ...task,
-                    done: !task.done
-                };
-            }
-            return task;
-        }))
-    };
-
-    const smoothCrossOutTask = (task) => {
-        const lengthTaskContent = task.content.length;
-        const result = `${((lengthTaskContent / 10) * 0.1).toFixed(1)}s`;
-        return result;
-    };
-
-    const removeTask = (id) => {
-        setTasks(tasks => tasks.filter(
-            task => task.id !== id
-        ))
-    };
+    const {
+        tasks,
+        hideDoneTasks,
+        addNewTask,
+        existTask,
+        hideAllDoneTasks,
+        completeAllTasks,
+        toggleDoneTask,
+        smoothCrossOutTask,
+        removeTask
+    } = useTasks();
 
     return (
         // Homework from the module 8 Frontend Developer
