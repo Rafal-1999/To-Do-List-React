b47bf97f4b34142a6881a23856fa8a8843fccd23
Rafal Chudy <rafalchu5@gmail.com>
2024-05-06 22:32:43 +0200

    New selectTasksByQuery selector added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -45,4 +45,13 @@ export const selectTasksState = (state) => state.tasks;
 export const selectTasks = (state) => selectTasksState(state).tasks;
 export const selectHideDone = (state) => selectTasksState(state).hideDone;
 export const getTaskById = (state, taskId) => selectTasks(state).find(({ id }) => id === taskId);
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) => content.toUpperCase().includes(query.toUpperCase().trim()));
+}
 export default tasksSlice.reducer;
